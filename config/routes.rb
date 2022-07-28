629f152e6af949a335f8ed630d54293e939dc5c0
marocks31 <mollyarisley@gmail.com>
2022-07-28 18:34:21 -0500

    created 5 RESTful API routes for crafts

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/crafts" => "crafts#index"
+  post "/crafts" => "crafts#create"
+  get "/crafts" => "crafts#show"
+  patch "/crafts/:id" => "crafts#update"
+  delete "/crafts/:id" => "crafts#destroy"
 end
