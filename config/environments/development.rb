ed51a7d661775567171bbf3abcfa114599922acc
marocks31 <mollyarisley@gmail.com>
2022-09-29 19:43:07 -0500

    set up gmail app password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,16 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'mollyarisley@gmail.com',
+    password:             Rails.application.credentials.gmail_password,
+    authentication:       'plain',
+  }
 
   config.action_mailer.perform_caching = false
 
